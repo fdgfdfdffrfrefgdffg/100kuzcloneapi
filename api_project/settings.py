6a085da4a092e8cb4f0a74f32580538631c2e167
fdgfdfdffrfrefgdffg <xudoyberganovodilbek6@gmail.com>
2024-09-29 20:50:46 +0500

    commit

--- api_project/settings.py
+++ api_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "rest_framework",
     "users",
+    'corsheaders',
     "posts",
     "images",
     "categories"
@@ -52,7 +53,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'api_project.urls'
 
