612447491bc4c8e8f37ef34357363886d9df01fc
fdgfdfdffrfrefgdffg <xudoyberganovodilbek6@gmail.com>
2024-10-04 08:26:59 +0500

    commit

--- api_project/settings.py
+++ api_project/settings.py
@@ -37,13 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "drf_yasg",
     "rest_framework",
     "users",
     'corsheaders',
     "posts",
     "images",
-    "categories",
-    "drf_yasg"
+    "categories"
 ]
 
 MIDDLEWARE = [
